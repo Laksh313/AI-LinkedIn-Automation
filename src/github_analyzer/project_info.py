5c2db4da36ef94c68787685ace3083d68a71dae7
Laksh <313.laksh@gmail.com>
2026-06-09 15:03:32 +0530

    intial commit

---
+++ src/github_analyzer/project_info.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class ProjectInfo(BaseModel):
+    name: str
+    description: str
+    languages: list[str]
+    tech_stack: list[str]
+    features:list[str]
+    learning_outcomes:list[str]
+
